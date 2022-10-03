3b10ac6a347897fa3ed15d02aa6dae65d3e65c08
Dmytro Zhyvov <dmytro.zhyvov@wikrgroup.com>
2022-10-03 22:09:27 +0300

    added lecture 15.2

--- cypress/e2e/cypressCommands/actionsOnElements.cy.js
+++ cypress/e2e/cypressCommands/actionsOnElements.cy.js
@@ -9,6 +9,45 @@ it('quering elements', () => {
 
     cy.get('#email1')
         .clear()
-        .type('SETT')
+        .type('S{leftArrow}E{leftArrow}T{rightArrow}{rightArrow}{rightArrow}T', {delay: 100})
 
+    cy.get('textarea[disabled]')
+        .type('bla bla', {force: true});
+
+    cy.get('#password1')
+        .focus()
+        .prev()
+        .should('have.attr', 'style')
+        .and('eq', 'color: orange;');
+
+    cy.get('#fullName1')
+        .click()
+        .blur()
+        .prev()
+        .should('have.attr', 'style')
+        .and('eq', 'color: red;');
+
+    cy.get('.action-form')
+        .type('text')
+        .submit()
+
+    cy.get('#action-canvas')
+        .click(15,15);
+
+    cy.get('.label.label-primary')
+        .click({multiple: true});
+
+    cy.get('.btn.btn-lg.btn-primary')
+        .click({force: true});
+
+    cy.get('.action-checkboxes [value="checkbox1"]')
+        .should('not.be.checked')
+        .check()
+        .should('be.checked')
+
+    cy.get('.action-checkboxes [value="checkbox2"]')
+        .should('not.be.checked')
+        .should('be.disabled')
+        .check({force: true})
+        .should('be.checked')
 })
